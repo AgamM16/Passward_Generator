e5a8e6c3819a2d6b02b5dbdc02e6ea45c48bd86d
AgamM16 <132464306+AgamM16@users.noreply.github.com>
2024-01-20 13:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def load_history():
         return []
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
